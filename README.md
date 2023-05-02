f8db64446d05f8a7242eac97a140f5fba67333c9
ymh3190 <ymh3190@gmail.com>
2023-05-02 14:44:56 +0900

    narrowing

--- README.md
+++ README.md
@@ -1,29 +1,44 @@
 # typescript-docs
 
-## 플래시카드
+플래시카드
 
-1. types by inference
-2. defining types
-3. union types
-4. handle different types too
-5. genertics
-6. own types that use generics
-7. typeof
-8. interface declaration with classes
-9. interface to annotate parameters and return values to functions
-10. typescript types
-11. typescript core principles => structural type system a.k.a duck typing and structural typing
-12. type checking shape-matching
-13. differences between type alias and interfaces
-14. type assertions
-15. liter types
-16. with union types
-17. numeric literal types
-18. literal interface
-19. strict null checks
-20. non-null assertion operator
-21. enums
-22. bigint(es2020)
-23. symbol
-24. narrowing
-25. typeof type guard
+```text
+    1. types by inference
+    2. defining types
+    3. union types
+    4. handle different types too
+    5. genertics
+    6. own types that use generics
+    7. typeof
+    8. interface declaration with classes
+    9. interface to annotate parameters and return values to functions
+    10. typescript types
+    11. typescript core principles => structural type system a.k.a duck typing and structural typing
+    12. type checking shape-matching
+    13. differences between type alias and interfaces
+    14. type assertions
+    15. liter types
+    16. with union types
+    17. numeric literal types
+    18. literal interface
+    19. strict null checks
+    20. non-null assertion operator
+    21. enums
+    22. bigint(es2020)
+    23. symbol
+    24. narrowing
+    25. typeof type guard
+    26. truthiness narrowing
+    27. equality narrowing
+    28. looser equality checks
+    29. 'in' operator narrowing
+    30. reiterate optional property
+    31. instanceof narrowing
+    32. assignment
+    33. control flow analysis
+    34. using type predicates
+    35. discriminated unions
+    36. switch statements
+    37. discriminated unions is crucial
+    38. exhaustiveness checking
+```
--- src/main.ts
+++ src/main.ts
@@ -6,14 +6,24 @@ function padLeft(padding: number | string, input: string) {
 }
 
 function printAll(strs: string | string[] | null) {
-  if (strs && typeof strs === "object") {
-    // null is object type...
-    for (const s of strs) {
-      console.log(s);
+  // null is object type...
+  // if (strs && typeof strs === "object") {
+  //   for (const s of strs) {
+  //     console.log(s);
+  //   }
+  // } else if (typeof strs === "string") {
+  //   console.log(strs);
+  // } else {
+  // }
+
+  if (strs !== null) {
+    if (typeof strs === "object") {
+      for (const s of strs) {
+        console.log(s);
+      }
+    } else if (typeof strs === "string") {
+      console.log(strs);
     }
-  } else if (typeof strs === "string") {
-    console.log(strs);
-  } else {
   }
 }
 
@@ -25,5 +35,84 @@ function multiplyAll(
     return values;
   } else {
     return values.map((x) => x * factor);
+  }
+}
+
+interface Container {
+  value: number | null | undefined;
+}
+function multiplyValue(container: Container, factor: number) {
+  if (container.value != null) {
+    console.log(container.value);
+    container.value *= factor;
+  }
+}
+
+type Fish = { swim: () => void; name: string };
+type Bird = { fly: () => void; name: string };
+function move(animal: Fish | Bird) {
+  if ("swim" in animal) {
+    return animal.swim();
+  }
+  return animal.fly();
+}
+
+type Human = { swim?: () => void; fly?: () => void };
+function move2(animal: Fish | Bird | Human) {
+  if ("swim" in animal) {
+    animal;
+  } else {
+    animal;
+  }
+}
+
+function isFish(pet: Fish | Bird): pet is Fish {
+  return (pet as Fish).swim !== undefined;
+}
+
+const zoo: (Fish | Bird)[] = [];
+const uderWater1: Fish[] = zoo.filter(isFish);
+const uderWater2: Fish[] = zoo.filter(isFish) as Fish[];
+const underWater3: Fish[] = zoo.filter((pet): pet is Fish => {
+  if (pet.name === "sharkey") return false;
+  return isFish(pet);
+});
+
+interface Shape {
+  kind: "circle" | "square";
+  radius?: number;
+  sideLength?: number;
+}
+
+function getArea(shape: Shape) {
+  if (shape.kind === "circle") {
+    return Math.PI * shape.radius! ** 2;
+  }
+}
+
+interface Circle {
+  kind: "circle";
+  radius: number;
+}
+
+interface Square {
+  kind: "square";
+  sideLength: number;
+}
+
+type Shape2 = Circle | Square;
+function getArea2(shape: Shape2) {
+  if (shape.kind === "circle") {
+    return Math.PI * shape.radius ** 2;
+  }
+}
+
+function getArea3(shape: Shape2) {
+  switch (shape.kind) {
+    case "circle":
+      return Math.PI * shape.radius ** 2;
+
+    case "square":
+      return shape.sideLength ** 2;
   }
 }
